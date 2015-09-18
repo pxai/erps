cf9d06034fb6f16f87d584e2945238b26516b0be
Administrador <Administrador@ORD-PROF-8.CUATROVIENTOS.local>
2015-09-18 10:15:23 +0200

    Fisrt commit for homework solution

---
+++ homework02/src/main/java/org/cuatrovientos/homework02/App.java
@@ -0,0 +1,11 @@
+package org.cuatrovientos.homework02;
+
+/**
+ * Solution to homework02
+ * Not done
+ */
+public class App {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+}
