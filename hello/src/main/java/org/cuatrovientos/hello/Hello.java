7800f66a7481c5d208337c1803a29f393637f02a
Administrador <Administrador@ORD-PROF-8.CUATROVIENTOS.local>
2015-09-21 10:19:09 +0200

    Added hello and usehello projects

---
+++ hello/src/main/java/org/cuatrovientos/hello/Hello.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package org.cuatrovientos.hello;
+
+/**
+ * Simple class to say hello
+ * @author Pello Altadill
+ *
+ */
+public class Hello {
+	public String hello () {
+		return "Hello Maven";
+	}
+}
---
+++ usehello/src/main/java/org/cuatrovientos/usehello/App.java
@@ -0,0 +1,15 @@
+package org.cuatrovientos.usehello;
+
+
+import org.cuatrovientos.hello.Hello;
+
+/**
+ * @author Pello Altadill
+ *
+ */
+public class App {
+	public static void main(String[] args) {
+		Hello myHello = new Hello();
+		System.out.println(myHello.hello());
+	}
+}
